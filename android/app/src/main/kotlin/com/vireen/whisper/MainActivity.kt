f897103b366401658a1a284c31e0860177cf69e2
lcp <liucaoping@doodod.com>
2024-01-11 19:45:57 +0800

    open file onclick message

--- android/app/src/main/kotlin/com/vireen/whisper/MainActivity.kt
+++ android/app/src/main/kotlin/com/vireen/whisper/MainActivity.kt
@@ -1,6 +1,12 @@
 package com.vireen.whisper
 
 import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.engine.FlutterEngine
+import io.flutter.plugins.GeneratedPluginRegistrant
 
-class MainActivity: FlutterActivity() {
+class MainActivity : FlutterActivity() {
+    override fun configureFlutterEngine(flutterEngine: FlutterEngine) {
+        GeneratedPluginRegistrant.registerWith(flutterEngine)
+        flutterEngine.plugins.add(DirPlugin())
+    }
 }
