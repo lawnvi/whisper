207c206cbf4b90201db449c14fe0d910cdd8640e
vireen <1340878220@qq.com>
2024-04-14 07:06:24 +0800

    update to 0.0.9

--- lib/page/deviceList.dart
+++ lib/page/deviceList.dart
@@ -1,7 +1,6 @@
 import 'dart:io';
 import 'package:bonsoir/bonsoir.dart';
 import 'package:flutter/cupertino.dart';
-import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_swipe_action_cell/core/cell.dart';
