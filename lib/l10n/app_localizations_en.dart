7edca6610e3b3061db23495543d6440f575400b6
lawnvi <1340878220@qq.com>
2026-04-21 11:14:05 +0800

    chore: polish english localization copy

--- lib/l10n/app_localizations_en.dart
+++ lib/l10n/app_localizations_en.dart
@@ -9,201 +9,202 @@ class AppLocalizationsEn extends AppLocalizations {
   AppLocalizationsEn([String locale = 'en']) : super(locale);
 
   @override
-  String get connectDeviceTitle => 'connect device';
+  String get connectDeviceTitle => 'Connect Device';
 
   @override
-  String get connectDeviceDesc => 'input ip&port';
+  String get connectDeviceDesc => 'Enter IP and Port';
 
   @override
-  String get connectTo => 'connect to';
+  String get connectTo => 'Connect To';
 
   @override
-  String get connectRequest => 'connect request';
+  String get connectRequest => 'Connection Request';
 
   @override
   String connectRequestDesc(String device) {
-    return 'new device: $device?';
+    return 'New device: $device?';
   }
 
   @override
-  String get connect => 'connect';
+  String get connect => 'Connect';
 
   @override
-  String get confirm => 'confirm';
+  String get confirm => 'Confirm';
 
   @override
-  String get allow => 'allow';
+  String get allow => 'Allow';
 
   @override
-  String get refuse => 'refuse';
+  String get refuse => 'Refuse';
 
   @override
-  String get cancel => 'cancel';
+  String get cancel => 'Cancel';
 
   @override
-  String get setting => 'setting';
+  String get setting => 'Settings';
 
   @override
-  String get sendTips => 'type something...';
+  String get sendTips => 'Type something...';
 
   @override
-  String get trust => 'trust device';
+  String get trust => 'Trust Device';
 
   @override
-  String get writeClipboard => 'write clipboard';
+  String get writeClipboard => 'Write to Clipboard';
 
   @override
-  String get deleteDevice => 'delete device';
+  String get deleteDevice => 'Delete Device';
 
   @override
   String serverPort(Object port) {
-    return 'server port $port';
+    return 'Server Port $port';
   }
 
   @override
-  String get serverPortTitle => 'server port';
+  String get serverPortTitle => 'Server Port';
 
   @override
-  String get trustNewDevice => 'auto access new device';
+  String get trustNewDevice => 'Auto-Approve New Device';
 
   @override
-  String get accessClipboard => 'access clipboard';
+  String get accessClipboard => 'Access Clipboard';
 
   @override
-  String get doubleClickRmMessage => 'delete message if double click message';
+  String get doubleClickRmMessage => 'Delete Message on Double Click';
 
   @override
-  String get close2tray => 'hide to tray when close application';
+  String get close2tray => 'Hide to Tray When Closing';
 
   @override
-  String get nickname => 'nickname';
+  String get nickname => 'Nickname';
 
   @override
-  String get nicknameDesc => 'input your nickname';
+  String get nicknameDesc => 'Enter your nickname';
 
   @override
-  String get port => 'port';
+  String get port => 'Port';
 
   @override
-  String get portDesc => 'port scope: [1000, 65535]';
+  String get portDesc => 'Port range: [1000, 65535]';
 
   @override
-  String get timeoutTitle => 'connect timeout';
+  String get timeoutTitle => 'Connection Timeout';
 
   @override
-  String get disconnect => 'disconnect';
+  String get disconnect => 'Disconnect';
 
   @override
-  String get keepConnect => 'keep';
+  String get keepConnect => 'Keep';
 
   @override
-  String get menuShow => 'show';
+  String get menuShow => 'Show';
 
   @override
-  String get menuHide => 'hide';
+  String get menuHide => 'Hide';
 
   @override
-  String get menuClipboard => 'send clipboard';
+  String get menuClipboard => 'Send Clipboard';
 
   @override
-  String get menuSendFile => 'send files';
+  String get menuSendFile => 'Send Files';
 
   @override
-  String get exit => 'exit';
+  String get exit => 'Exit';
 
   @override
-  String get delete => 'delete';
+  String get delete => 'Delete';
 
   @override
-  String get deleteConfirm => 'delete confirm';
+  String get deleteConfirm => 'Confirm Delete';
 
   @override
-  String get warning => 'warning';
+  String get warning => 'Warning';
 
   @override
-  String get deleteWarningText => 'connect is alive, refuse delete in fast';
+  String get deleteWarningText =>
+      'The connection is still active, so quick delete is blocked';
 
   @override
-  String get close => 'close';
+  String get close => 'Close';
 
   @override
   String deleteDeviceTitle(String device) {
-    return 'delete $device';
+    return 'Delete $device';
   }
 
   @override
   String get deleteDeviceDesc =>
-      'clear any messages about this device, it\'s not recoverable!';
+      'Clear all messages for this device. This cannot be undone.';
 
   @override
-  String get brokeConnectTitle => 'disconnect';
+  String get brokeConnectTitle => 'Disconnect';
 
   @override
   String brokeConnectDesc(String device) {
-    return 'disconnect $device';
+    return 'Disconnect $device';
   }
 
   @override
-  String get connectFailed => 'connect error';
+  String get connectFailed => 'Connection Failed';
 
   @override
-  String get deviceBusy => 'device busy';
+  String get deviceBusy => 'Device Busy';
 
   @override
-  String get startServerFailed => 'server not start';
+  String get startServerFailed => 'Failed to Start Server';
 
   @override
-  String get deleteMessageTitle => 'delete message';
+  String get deleteMessageTitle => 'Delete Message';
 
   @override
-  String get deleteMessageDesc => 'are you sure delete it?';
+  String get deleteMessageDesc => 'Are you sure you want to delete it?';
 
   @override
   String language(Object language) {
-    return 'language $language';
+    return 'Language $language';
   }
 
   @override
-  String get pushNotification => 'push android notification';
+  String get pushNotification => 'Forward Android Notifications';
 
   @override
-  String get ignoreNotification => 'ignore android notification';
+  String get ignoreNotification => 'Ignore Android Notifications';
 
   @override
   String get ftpService => 'FTP Service';
 
   @override
-  String get back => 'back';
+  String get back => 'Back';
 
   @override
-  String get selectAll => 'all';
+  String get selectAll => 'Select All';
 
   @override
-  String get clearAll => 'clear';
+  String get clearAll => 'Clear';
 
   @override
-  String get selectNotifyApp => 'listen notify app';
+  String get selectNotifyApp => 'Listen to App Notifications';
 
   @override
-  String get copyVerifyCode => 'verify code to clipboard';
+  String get copyVerifyCode => 'Copy Verification Code to Clipboard';
 
   @override
-  String get open => 'open';
+  String get open => 'Open';
 
   @override
-  String get openInFinder => 'open in Finder';
+  String get openInFinder => 'Open in Finder';
 
   @override
-  String get openInDir => 'open directory';
+  String get openInDir => 'Open Directory';
 
   @override
-  String get keepFile => 'keep file';
+  String get keepFile => 'Keep File';
 
   @override
-  String get deleteFile => 'delete file';
+  String get deleteFile => 'Delete File';
 
   @override
-  String get copyMessage => 'copy message content';
+  String get copyMessage => 'Copy Message Content';
 
   @override
   String get themeMode => 'Theme Mode';
